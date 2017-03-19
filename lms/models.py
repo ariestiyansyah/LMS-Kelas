f6fe4489508e7e59d6d0b53c4a7c0e35771a17b8
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2017-03-19 23:50:43 +0700

    Models for content

--- lms/models.py
+++ lms/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Judul(models.Model):
     title = models.CharField(max_length=100) # Title of the course
@@ -33,3 +35,33 @@ class Modul(models.Model):
 
     def __str__(self):
         return self.title
+
+class Konten(models.Model):
+    module = models.ForeignKey(Modul, related_name='contents')  # Define ForeignKey to Modul model
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField() # Store pimary key
+    item = GenericForeignKey('content_type', 'object_id') # Generic relation to associate objects from different models
+
+class KontenBase(models.Model):
+    xman = models.ForeignKey(User, related_name='%(class)s_related')
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(autp_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+class Teks(KontenBase):
+    konten = models.TextField()
+
+class File(KontenBase):
+    file = models.FileField(upload_to='files')
+
+class Gambar(KontenBas):
+    file = models.FileField(upload_to='images')
+
+class Video(KontenBase):
+    url = models.URLField()
