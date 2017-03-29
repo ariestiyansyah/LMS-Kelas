1a1a06abe0029c1d85ddb2cd9d08121101a02c6b
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2017-03-29 14:20:46 +0700

    typo urlresolvers

--- kelasx/settings.py
+++ kelasx/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.core.urlresolver import reverse_lazy
+from django.core.urlresolvers import reverse_lazy
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
